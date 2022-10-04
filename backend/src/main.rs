d9b3561d3dc16de1900cec57248387892dbc2b66
kyunghyunHan <ekfqlwcjswl@naver.com>
2022-10-04 09:55:54 +0900

    add

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,27 +1,36 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
+use actix_cors::Cors;
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result};
+use serde::Serialize;
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[derive(Serialize)]
+struct MyObj {
+    data: String,
 }
+#[get("/ed")]
+async fn index() -> Result<impl Responder> {
+    let obj = MyObj {
+        data: "Hello in Rust".to_string(),
+    };
+    Ok(web::Json(obj))
+}
+
 #[actix_web::main]
+
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allowed_methods(vec!["POST", "PUT", "PATCH", "GET", "OPTIONS", "HEAD"]);
+
         App::new()
-            .service(hello)
+            .wrap(cors) // 해결!
+            .service(index)
             .service(echo)
-            .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind("127.0.0.1:8000")?
     .run()
     .await
 }
---
+++ frontend/src/App.js
@@ -0,0 +1,86 @@
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [userName, setUserName] = useState("");
+  const [Data, setData] = useState("");
+
+  const nameHandler = (e) => {
+    e.preventDefault();
+    setUserName(e.target.value);
+  };
+  //create
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    let body = {
+      name: userName,
+      identity: "1",
+      hometown: "2",
+      age: 29,
+    };
+
+    const json = JSON.stringify(body);
+
+    await axios
+      .post("http://localhost:8000/join", json)
+      .then((res) => console.log(res))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  //delete
+  const delete_test = async () => {
+    await axios
+      .delete("http://localhost:8000/delete/4", {
+        name: userName,
+        identity: "1",
+        hometown: "2",
+        age: 29,
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  //read
+  const read = async () => {
+    await axios
+      .get("http://localhost:8000/ed", {})
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  //update
+  const update = async () => {
+    await axios
+      .post("http://localhost:8000/read", {})
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="App">
+      <form onSubmit={submitHandler}>
+        이름 : <input type="text" value={userName} onChange={nameHandler} />
+        <button type="submit">전송(create)</button>
+      </form>
+      <button onClick={read}>read</button>
+      <button onClick={delete_test}>delete</button>
+      <button onClick={update}>update</button>
+      <div>read:{Data[0]}</div>
+    </div>
+  );
+}
+
+export default App;
