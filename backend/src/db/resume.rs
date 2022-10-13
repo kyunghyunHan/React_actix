39ac58c0a59c69d1b00a1c113f87ca0de1dc426f
kyunghyunHan <ekfqlwcjswl@naver.com>
2022-10-13 10:51:11 +0900

    ㅁㅇㅇ

--- backend/src/db/resume.rs
+++ backend/src/db/resume.rs
@@ -6,5 +6,8 @@ pub struct Resume {
     pub Cv_email: String,
     pub Cv_letter: String,
     pub Cv_tech: String,
+    pub Cv_Edu: String,
+    pub Cv_Cert: String,
+    pub CV_Awards: String,
     pub created_at: chrono::NaiveDateTime,
 }
